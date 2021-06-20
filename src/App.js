525cfc1c47e634ce6705991f788af22c4c5fbb5e
luisrov <luisrov89@gmail.com>
2021-06-20 22:25:18 +0200

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import {IntlProvider, FormattedMessage} from 'react-intl';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import './App.css';
+import Main from './components/main/main.jsx';
+import Details from './components/details/details.jsx';
+import messages_en from './translations/en.json';
+import messages_es from './translations/es.json';
+import {Container, Row, Col} from 'react-bootstrap';
+import Select from 'react-select'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const messages = {
+  'es': messages_es,
+  'en': messages_en
+};
+
+function App() {
+  const [language, setLanguage] = useState(navigator.language.split(/[-_]/)[0]);
+  let changeLanguage = (lang) => {
+    setLanguage(lang.value);
+  }
+
+  return (
+    <IntlProvider locale={language} messages={messages[language]}>
+        <header className="App-header">
+          <Container>
+            <Row>
+              <Col xs={11} className="App-title">
+                <FormattedMessage id="title" />
+              </Col>
+              <Col xs={1} className="box-lang-select">
+                <Select className="lang-select"
+                  defaultValue={{value: language, label: language.toUpperCase()}}
+                  options={[{ value: 'es', label: 'ES' }, { value: 'en', label: 'EN' }]} 
+                  onChange={value => changeLanguage(value)}
+                />
+              </Col>
+            </Row>
+          </Container>
+        </header>  
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Main />
+            </Route>
+            <Route path="/details/:charId/:charName">
+              <Details />
+            </Route>
+          </Switch>
+        </Router>
+        <div>
+          
+        </div> 
+    </IntlProvider>
+  );
+}
+
+export default App;
---
+++ src/components/details/details.jsx
@@ -0,0 +1,77 @@
+import React, {useState, useEffect} from 'react';
+import { useHistory, useParams} from "react-router-dom";
+import {Container, Col, Row, Button} from 'react-bootstrap';
+import './details.css';
+import {FormattedMessage, useIntl} from 'react-intl';
+import svgLoading from '../../assets/loading.svg';
+
+const Details = () => {
+    const [data, setData] = useState(null);
+    const [quote, setQuote] = useState(null);
+    const [status, setStatus] = useState('loading');
+    const history = useHistory();
+    const { charId, charName} = useParams();
+    const intl = useIntl()
+    
+    useEffect(() => {
+        fetch(`https://breakingbadapi.com/api/characters/${charId}`).then(response => response.json()).then(data => {
+            setData(data[0]);
+            setStatus('success');
+    }).catch(() => setStatus('error'));
+        getRandomQuote();
+    }, []);
+
+    const getRandomQuote = () => {
+        fetch(`https://breakingbadapi.com/api/quote/random?author=${charName}`).then(response => response.json()).then(data => setQuote(data[0]?.quote));
+    }
+
+    return (
+        status === 'loading' ?
+        <Container className="data-loading">
+            <img src={svgLoading} alt='svg not found' />
+        </Container>:
+        status === 'success' ?
+        <Container>
+            <Row className="header-details">
+                <Button variant="primary" onClick={() => {history.push('/')}}>
+                    <FormattedMessage id='backToMain' />
+                </Button>
+            </Row>
+            <Row>
+                <Col md={6}>
+                    <div className="box">
+                        <img src={data.img} alt='svg not found' />
+                    </div>
+                    
+                </Col>
+                <Col md={6}>
+                    <p><text className="label-details"><FormattedMessage id='name' /></text><text>{data.name}</text></p>
+                    <p><text className="label-details"><FormattedMessage id='nickname' /></text><text>{data.nickname}</text></p>
+                    <p><text className="label-details"><FormattedMessage id='status' /></text><text>{data.status}</text></p>
+                    <p><text className="label-details"><FormattedMessage id='birthday' /></text><text>{data.birthday}</text></p>
+                    <p><text className="label-details"><FormattedMessage id='occupation' /></text><text>{data.occupation}</text></p>
+                    <p><text className="label-details"><FormattedMessage id='portrayed' /></text><text>{data.portrayed}</text></p>
+                    <p><text className="label-details"><FormattedMessage id='appearance' /></text><text>{data.appearance}</text></p>
+                    <p><text className="label-details"><FormattedMessage id='better_call_saul_appearance' /></text><text>{data.better_call_saul_appearance}</text></p>
+                    <p><text className="label-details"><FormattedMessage id='category' /></text><text>{data.category}</text></p>
+                    <p><text className="label-details"><FormattedMessage id='char_id' /></text><text>{data.char_id}</text></p>
+                    <p>
+                        <text>{quote? `-${charName}: ${quote}`: intl.formatMessage({id: 'quoteNotFound'})}</text>
+                    </p>
+                    <Button variant="info" onClick={() => {getRandomQuote()}}>
+                        <FormattedMessage id='changeQuote' />
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+        :<Container>
+            <Button variant="primary" onClick={() => {history.push('/')}}>
+                <FormattedMessage id='backToMain' />
+            </Button>
+            <h1><FormattedMessage id='charNotFound' /></h1>
+        </Container>
+    );
+
+};
+
+export default Details;
