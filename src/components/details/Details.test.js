8fa439efd6aa268b5ec16e63a12eef84cb8d8547
luisrov <luisrov89@gmail.com>
2021-06-21 21:17:59 +0200

    config review

--- src/components/details/Details.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Details from './Details';
-
-test('renders Details', () => {
-  main = render(<Details />);
-});
-//TODO testear componente
--- src/components/main/Main.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Main from './Main';
-
-test('renders Main', () => {
-  main = render(<Main />);
-});
-//TODO testear componente
